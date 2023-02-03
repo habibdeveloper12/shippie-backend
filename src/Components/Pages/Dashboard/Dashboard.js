32b95616e748dcc7592980eeee9e1c4a98ad8199
Haider Ali <73281382+nothaiderali@users.noreply.github.com>
2023-02-03 20:20:17 +0500

    Added functionality to Dashboard

--- src/Components/Pages/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard.js
@@ -1,40 +1,88 @@
-import React from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import './dashboard.css'
-import Order from './Order';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "./dashboard.css";
+import Order from "./Order";
 const Dashboard = () => {
-  
+  let userid = "123456789";
+
+  let loading = (
+    <div className="grid items-center justify-center py-40">
+      <b className="flex justify-center">Loading . . .</b>
+    </div>
+  );
+
+  let nodata = (
+    <div className="grid items-center justify-center py-40">
+      <b className="flex justify-center">No transactions Found</b>
+    </div>
+  );
+
+  let fetchData = () => {
+    fetch("http://localhost:5000/api/orders/" + userid)
+      .then((response) => response.json())
+      .then((json) => sortOrders(json))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => fetchData(),[]);
+
+  const [tab0, setTab0] = useState(loading);
+  const [tab1, setTab1] = useState(loading);
+  const [tab2, setTab2] = useState(loading);
+  const [tab3, setTab3] = useState(loading);
+  const [tab4, setTab4] = useState(loading);
+  let box = (c) => {
+    return c;
+  };
+
+  let sortOrders = (json) => {
+    let sorted = [[], [], [], [], []];
+    sorted[0] = json;
+    json.map((curElem) => {
+      sorted[parseInt(curElem.status)].push(curElem);
+    });
+    sorted.map((curElem, i) => {
+      if (curElem.length !== 0) {
+        if (i === 0) setTab0(curElem.map((c) => <Order key={c._id} {...c} />));
+        else if (i === 1) setTab1(curElem.map((c, i) => <Order key={c._id} {...c} />));
+        else if (i === 2) setTab2(curElem.map((c, i) => <Order key={c._id} {...c} />));
+        else if (i === 3) setTab3(curElem.map((c, i) => <Order key={c._id} {...c} />));
+        else if (i === 4) setTab4(curElem.map((c, i) => <Order key={c._id} {...c} />));
+      } else eval("setTab" + i + "(nodata)");
+    });
+    // console.log(sorted);
+  };
+
+  let tabs = (
+    <>
+      <TabPanel>{tab0}</TabPanel>
+      <TabPanel>{tab1}</TabPanel>
+      <TabPanel>{tab2}</TabPanel>
+      <TabPanel>{tab3}</TabPanel>
+      <TabPanel>{tab4}</TabPanel>
+    </>
+  );
+
   return (
-      <div className="container mx-auto">
-        <h2 className='font-black text-5xl text-center'>My Orders</h2>
-      <Tabs defaultIndex={1} onSelect={(index) => console.log(index)}>
-        <TabList className={" flex justify-start items-start gap-6  py-1 text-grey mt-4"}>
+    <div className="container mx-auto">
+      <h2 className="font-black text-5xl text-center mt-4">My Orders</h2>
+      <Tabs defaultIndex={1} /*onSelect={(index) => console.log(index)}*/>
+        <TabList
+          className={
+            " flex justify-start items-start gap-6 py-1 text-grey mt-4"
+          }
+        >
           <Tab className={"cursor-pointer text-xl font-bold"}>All</Tab>
-          <Tab className={"cursor-pointer text-xl font-bold"}>Pending your action</Tab>
-          <Tab className={"cursor-pointer text-xl font-bold"}>in Progress</Tab>
-          <Tab className={"cursor-pointer text-xl font-bold"}>completed</Tab>
+          <Tab className={"cursor-pointer text-xl font-bold"}>
+            Action Pending
+          </Tab>
+          <Tab className={"cursor-pointer text-xl font-bold"}>In Progress</Tab>
+          <Tab className={"cursor-pointer text-xl font-bold"}>Completed</Tab>
           <Tab className={"cursor-pointer text-xl font-bold"}>Cancelled</Tab>
         </TabList>
-        <hr  className='w-[100%] bg-slate-200 h-[4px] mb-3  '/>
-        <TabPanel>
-         <Order/>
-     </TabPanel>
-    
-        <TabPanel>
-          
-             <Order/>
-            </TabPanel>
-        <TabPanel>
-          
-              <p>Alien from Decapod 10. Planet Express' staff doctor and steward. Has a medical degree and Ph.D in art history.</p>
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Dr_John_Zoidberg.png/200px-Dr_John_Zoidberg.png" alt="Doctor John Zoidberg" />
-            </TabPanel>
-        <TabPanel>
-          
-              <p>Alien from Decapod 10. Planet Express' staff doctor and steward. Has a medical degree and Ph.D in art history.</p>
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Dr_John_Zoidberg.png/200px-Dr_John_Zoidberg.png" alt="Doctor John Zoidberg" />
-            </TabPanel>
-         
+        <hr className="w-[100%] bg-slate-400 h-[4px] mb-3 border-0" />
+        {tabs}
       </Tabs>
     </div>
   );
--- src/Components/Pages/Dashboard/Order.js
+++ src/Components/Pages/Dashboard/Order.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
 
-const Order = () => {
+const Order = (p) => {
+  // console.log(p);
+
   return (
     <div>
-      <a className="md:grid md:grid-cols-3 flex flex-col w-full h-40 mt-6 bg-white hover:bg-gray-1 rounded-md shadow-md hover:shadow-lg items-center justify-center truncate" href="/dashboard/orders/221025A001R01"><div className="md:col-span-2 relative flex flex-col justify-center w-full items-center md:h-40 px-8 md:px-12 md:border-r border-border-gray"><div className="w-full text-center md:text-left"><h2 className="text-base lg:text-lg font-bold overflow-hidden text-ellipsis">To Ddd, FI</h2><p className="text-xs md:text-vsm text-gray-4">25 Oct 2022, 4:24 AM</p></div><div className="md:absolute w-fit md:w-1/3 min-w-fit py-0 md:py-0.5 left-0 top-0 px-2 md:px-2.5 rounded-md md:rounded-none md:rounded-br-md whitespace-pre bg-background-dark-gray text-subheading-gray"><span className="text-sm md:text-xs">Order</span><span className="text-sm md:text-base tracking-wide"> 221025A001R01</span></div></div><div className="md:col-span-1 mt-4 md:mt-0 md:px-12 text-center md:text-left"><span className="text-sm md:text-base font-bold flex items-center text-subheading-gray"><svg xmlns="http://www.w3.org/2000/svg" className="w-4 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg><span className="ml-2">Cancelled</span></span></div></a>
+      <div className="text-slate-700 md:grid md:grid-cols-3 flex flex-col w-full h-40 my-6 bg-white hover:bg-gray-1 rounded-md shadow-md hover:shadow-lg items-center justify-center truncate">
+        <div className="md:col-span-2 relative flex flex-col justify-center w-full items-center md:h-40 px-8 md:px-12 md:border-r border-border-gray">
+          <div className="w-full text-center md:text-left">
+            <p className="text-base lg:text-lg overflow-hidden text-ellipsis">
+              <b>Sender:</b> {p.sender.firstName + " " + p.sender.lastName}
+              <br />
+              <b>Recipient:</b> {p.recipient.name}
+              <br />
+              <b>Status:</b> {["Action Pending","In Progress","Completed","Cancelled"][p.status-1]}
+              <br />
+              <b>Date and Time:</b> {new Date(p.createdAt).toLocaleString()}
+              <br />
+              <b>Order ID:</b> {p._id}
+            </p>
+          </div>
+        </div>
+        <div className="md:col-span-1 mt-4 md:mt-0 md:px-12 text-center grid justify-items-center">{p.status == 1 ? <>
+        <a
+          href={p.paymentLink}
+          className="text-sm md:text-base font-bold flex items-center p-1 text-purple-600"
+        >
+          Goto Checkout
+        </a>
+        <a
+          href={"/dashboard/orders/" + p._id}
+          className="text-sm md:text-base font-bold flex items-center p-1"
+        >
+          Remove from Cart
+        </a>
+        <a
+          href={"/dashboard/orders/" + p._id}
+          className="text-sm md:text-base font-bold flex items-center p-1"
+        >
+          Get all Details
+        </a>
+      </> :<>
+        <a
+          href={"/dashboard/orders/" + p._id}
+          className="text-sm md:text-base font-bold flex items-center p-1"
+        >
+          Get all Details
+        </a>
+      </>}</div>
+      </div>
     </div>
   );
 };
