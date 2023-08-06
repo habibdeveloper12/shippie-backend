60351392a12f28f854574fc4f0d65694c7a83960
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-08-06 19:45:20 +0600

    dfd

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { APP_PORT, DB_URL } from "./config/index.js";
 import errorHandler from "./middlewares/errorHandler.js";
 const app = express();
 import routes from "./routes/index.js";
-
+import bodyParser from "body-parser";
+import crypto from "crypto-js";
 import CryptoJS from "crypto-js";
 function conversion(secret, message) {
   var hash = CryptoJS.HmacSHA1(message, secret);
@@ -26,7 +27,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("DB connected...");
 });
-
+app.use(bodyParser.json());
 global.appRoot = path.resolve("./");
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -66,8 +67,8 @@ app.use((req, res, next) => {
 //   const description = `Shipment by Habib`;
 //   const currency = "SGD";
 //   const amount = 10;
-//   const order_id = "uuid_1_uuid";
-//   const user_fullname = "Michael Scott";
+//   const order_id = "usdfs1id";
+//   const user_fullname = "sdf sdf";
 //   const secret_key = "OGVQ4KW90AMBRR5YA34YPLDI3ZJJANGU";
 //   const hashString = `${cust_code}${merchant_outlet_id}${terminal_id}${merchant_return_url}${description}${currency}${amount}${order_id}${user_fullname}`;
 //   const hash = CryptoJS.HmacSHA1(hashString, secret_key);
@@ -97,6 +98,74 @@ app.use((req, res, next) => {
 // };
 
 // sendPaymentRequest();
+
+function serializeObjectByOrder(obj, order) {
+  const serialized = {};
+  order.forEach((key) => {
+    if (obj.hasOwnProperty(key)) {
+      serialized[key] = obj[key];
+    }
+  });
+  return serialized;
+}
+
+app.post("/request-payment", async (req, res) => {
+  const data = req.body;
+  // const data = {
+  //   cust_code: "001098",
+  //   merchant_outlet_id: "01",
+  //   merchant_return_url: "https://google.com",
+  //   terminal_id: "001",
+  //   description: "ghghgh",
+  //   currency: "SGD",
+  //   amount: 1000,
+  //   order_id: "uuid_1_uufghfg5ghghidsd",
+  //   user_fullname: "Michael Scott",
+  //   user_email: "userEmail@test.com",
+  // };
+
+  data.hash = generateHMAC(data, "OGVQ4KW90AMBRR5YA34YPLDI3ZJJANGU");
+
+  const apiUrl = "https://portalapi.oisbizcraft.com/api/payments";
+
+  try {
+    const response = await axios.post(apiUrl, data);
+    res.json(response.data);
+    console.log("Response from OIS-Bizcraft API:", response.data);
+    // Handle the response from the OIS-Bizcraft API here as needed
+  } catch (error) {
+    console.error("Error calling OIS-Bizcraft API:", error);
+    // Handle errors here if the API call fails
+  }
+});
+
+function generateHMAC(data, secretKey) {
+  const string =
+    data.cust_code +
+    data.merchant_outlet_id +
+    data.terminal_id +
+    data.merchant_return_url +
+    data.description +
+    data.currency +
+    data.amount +
+    data.order_id +
+    data.user_fullname;
+
+  const hmac = CryptoJS.HmacSHA256(string, secretKey);
+  return hmac.toString().toUpperCase();
+}
+function generateRandomStr(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+// Call the function to make the API request
+// postToOISBizcraftApi();
 
 app.use(express.static("./public", { maxAge: 86400000 }));
 app.use(fallback("index.html", { root: "./public" }));
