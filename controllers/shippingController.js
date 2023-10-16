98aeb5d782a93c8d801afa8ecff6e3bad6ea4fd3
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-10-17 05:45:21 +0600

    critical

--- controllers/shippingController.js
+++ controllers/shippingController.js
@@ -225,6 +225,145 @@ const shippingController = {
       }
     });
   },
+  async QouteShipping(req, res) {
+    console.log(req.body);
+    ("");
+    const { packages, country } = req.body;
+    // const { recipient, packages, sender, addons, date } = req.body;
+    // const { country: senderCountry } = sender;
+    // const { quantity, description, category, value, weight } =
+    //   req.body.packages[0].items[0];
+    // console.log("sender", sender);
+
+    function getRandomPostalCode(countryCode) {
+      const countryPostalCodes = postalCodes[countryCode];
+      if (!countryPostalCodes || countryPostalCodes.length === 0) {
+        return "Postal codes not available for this country.";
+      }
+      const randomIndex = Math.floor(Math.random() * countryPostalCodes.length);
+      return countryPostalCodes[randomIndex];
+    }
+    try {
+      const stateResponse = await axios.get(
+        `https://app.zipcodebase.com/api/v1/country/province?apikey=a6618ae0-6c69-11ee-b32f-1dbde90525b8&country=${country}`
+      );
+      const state = stateResponse.data.results[0];
+      console.log("Response from OIS-Bizcraft API:", state);
+      const postalResponse = await axios.get(
+        `https://app.zipcodebase.com/api/v1/code/state?apikey=a6618ae0-6c69-11ee-b32f-1dbde90525b8&state_name=${state}&country=${country}&limit=10`
+      );
+      let postal_code = postalResponse.data.results[0];
+      if (!postal_code) {
+        postal_code = 79701;
+        country = "US";
+      }
+      console.log(
+        "Response from OIS-Bizcraft API:",
+        postalResponse.data.results[0]
+      );
+      const requestedPackageLineItems = packages.map((p) => {
+        return {
+          weight: {
+            units: "KG",
+            value: +p.box.weight,
+          },
+          dimensions: {
+            length: +p.box.length,
+            width: +p.box.width,
+            height: +p.box.height,
+            units: "CM",
+          },
+        };
+      });
+      const shippingData = {
+        accountNumber: {
+          value: "121059770",
+        },
+        rateRequestControlParameters: {
+          returnTransitTimes: true,
+        },
+        requestedShipment: {
+          shipper: {
+            address: {
+              postalCode: "486058",
+              // postalCode: 1201,
+              countryCode: "SG",
+            },
+          },
+          recipient: {
+            address: {
+              postalCode: postal_code,
+              countryCode: country,
+            },
+          },
+          pickupType: "DROPOFF_AT_FEDEX_LOCATION",
+          rateRequestType: ["LIST", "ACCOUNT"],
+          customsClearanceDetail: {
+            dutiesPayment: {
+              paymentType: "SENDER",
+              payor: {
+                responsibleParty: null,
+              },
+            },
+            commodities: [
+              {
+                description: " GOod for kids computers",
+                quantity: 1,
+                quantityUnits: "PCS",
+                weight: {
+                  units: "KG",
+                  value: 2,
+                },
+                customsValue: {
+                  amount: 5,
+                  currency: "USD",
+                },
+              },
+            ],
+          },
+          requestedPackageLineItems: requestedPackageLineItems,
+        },
+      };
+      try {
+        const authResponse = await axios.post(
+          "https://apis.fedex.com/oauth/token",
+          {
+            grant_type: "client_credentials",
+            client_id: "l705f47025631443e39c111c0956a335c5",
+            client_secret: "3ae0c814-6b79-46e3-b9d6-e7849ef09ef7",
+          },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+
+        const accessToken = authResponse.data.access_token;
+        console.log("Access", accessToken);
+        const rateResponse = await axios.post(
+          "https://apis.fedex.com/rate/v1/rates/quotes",
+          shippingData,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        // console.log("ship,", rateResponse)
+        const shippingRate = rateResponse.data.output.rateReplyDetails;
+        console.log(shippingRate);
+        res.json({ shippingRate });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+      }
+      // Handle the response from the OIS-Bizcraft API here as needed
+    } catch (error) {
+      console.error("Error calling OIS-Bizcraft API:", error);
+      // Handle errors here if the API call fails
+    }
+  },
 };
 
 export default shippingController;
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.put("/shippings/:id", shippingController.setPaymentLink);
 router.get("/shippings/:id", shippingController.show);
 router.get("/orders/:id", shippingController.orders);
 router.post("/getShippingRate", shippingController.shippingRate);
+router.post("/getqoute", shippingController.QouteShipping);
 router.post("/request-payment", shippingController.RateOisPayment);
 router.post("/contact", shippingController.Contact);
 
